a442b5bada1759b0e3aa92dec4c673e5feb321c8
LSG2024 <156012256+LSG2024@users.noreply.github.com>
2024-04-12 05:09:43 +0000

    Spencer Garcia

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,34 @@
+import React from "react";
+import { Roboto } from "next/font/google";
+import styles from "./page2.css"
+import "../globals.css";
+
+
+
 export default function Home() {
   return (
-    <main>
-      About
-    </main>
+    <section className="container">
+      <div className="content">
+        <h1 className="title">I am Spencer Garcia a College Student</h1>
+        <p className="description">
+          I am from Bolivia, but I currently live in Utah, United States. I am 24 years with 2 years on college doing a Bachelor's of Software development. Almost finishing my first certificate in software engineering, I have some experience in Java but with more skill in JavaScript. With good wishes to continue expanding my skills and knowledge in the world of technology and programming to make this a better world.
+        </p>
+        <p className="description2">
+          Any way to contact me, there is my Linkedin account and my Github, also my Gmail.
+        </p>
+      </div>
+      <div className="right-container">
+        <img src="/AboutPrfl.png" alt="My prof picture" className="myImg" />
+        <div className="btn-container">
+          <img src="/Github.png" alt="G picture" className="Git" />
+          <a href="https://github.com/LSG2024" className="contactBtn">Follow me on Github</a>
+          <img src="/Linkedin.png" alt="Link picture" className="Link" />
+          <a href="https://www.linkedin.com/in/spencer-garcia-b79b80252?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BVSkQubQaQyG5Yb4jnc0P7g%3D%3D" className="contactBtn2">Follow me on Linkedin</a>
+          <img src="/Gmail.png" alt="My prof picture" className="myImg" />
+          <a href="mailto:luispencerg@gmail.com" className="contactBtn3">Send me an email</a>
+        </div>
+      </div>
+    </section>
   );
 }
+
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,58 @@
 import Image from 'next/image';
 import styles from "./page.css"
 import global from "./globals.css"
+import WorkWidget from "../components/WorkWidget/WorkWidget"
+import SkillsWidget from "../components/SkillsWidget/SkillsWidget"
+
+const skills = [
+  { name: 'Microsoft', proficiency: 60, icon: 'microsoft.png' },
+  { name: 'Java', proficiency: 50, icon: 'Java.png' },
+  { name: 'JavaScript', proficiency: 75, icon: 'javaS.png' },
+];
 
 export default function Home() {
   return (
     <section className= "container">
       <div className= "content">
-        <h1 className= "Title">Hi, I am Spencer</h1>
-        <p className= "Description">
+        <h1 className= "title">Hi, I am Spencer</h1>
+        <p className= "description">
           I am a Software developer 
-          </p>
-          <a href= "mailto:luispencerg@gmail.com" className= "contactBtn">Contact me</a>
+        </p>
+        <a href= "mailto:luispencerg@gmail.com" className= "contactBtn">Contact me</a>
+        <WorkWidget
+          title={'Studies '}
+          content={'My education experience.'}
+          experiences={[
+            {
+              logo: '/EnsignCollege.png',
+              organization: 'Ensign college',
+              jobTitle: 'Software Engineering',
+              startYear: 2022,
+              endYear: null,
+            },
+            {
+              logo: '/Byu.png',
+              organization: 'BYU',
+              jobTitle: 'English Language',
+              startYear: 2020,
+              endYear: 2022,
+            },
+            {
+              logo: '/path.png',
+              organization: 'Pathway Connect',
+              jobTitle: 'Bussiness Management',
+              startYear: 2020,
+              endYear: 2020,
+            },
+            
+          ]}
+        />
+        <SkillsWidget
+        title={'Skills'}
+        content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi.'}
+        skills={skills}
+      />
+        
       </div>
       <img src= "/prof.png" alt= "My prof picture" className= "myImg"/>
     </section>
